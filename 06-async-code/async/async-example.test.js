182eabfe65d413643aa3033f4079a7d86fe396fa
Yong <fkszm3@gmail.com>
2022-09-26 23:08:02 +0900

    06-50 Testing Asynchronous Code With Callbacks

--- 06-async-code/async/async-example.test.js
+++ 06-async-code/async/async-example.test.js
@@ -1,14 +1,23 @@
 import { expect, it } from "vitest";
 import { generateToken } from "./async-example";
 
-it("토근 값을 생성해낸다.", () => {
+it("토근 값을 생성해낸다.", (done) => {
   const userEmail = "test@test.com";
 
   generateToken(userEmail, (err, token) => {
     // expect(token).toBeDefined(); // Undefined가 아닌것을 기대함
-    expect(token).toBe(2);
+
+    try {
+      expect(token).toBeDefined();
+      // expect(token).toBe(2);
+      done();
+    } catch (error) {
+      done(error);
+    }
+
     // toBe(2) 도 테스트에 통과하고 있고,
     // 실제로 작동하는 테스트 코드가 아니라고 봐야함.
     // 고쳐보자!
+    // done();
   });
 });
