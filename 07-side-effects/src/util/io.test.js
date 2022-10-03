9e729111f033a3bd6873b5d3b846655cb6ec0ce4
Yong <fkszm3@gmail.com>
2022-10-03 17:17:02 +0900

    07-64 Getting Started with Mocks & Automocking

--- 07-side-effects/src/util/io.test.js
+++ 07-side-effects/src/util/io.test.js
@@ -1,10 +1,16 @@
-import { it, expect } from "vitest";
+import { it, expect, vi } from "vitest";
+import { promises as fs } from "fs";
 
 import writeData from "./io";
+
+vi.mock("fs");
 
 it("함수를 실행하여 파일을 생성한다.", () => {
   const testData = "Test";
   const testFileName = "test.txt";
-  
-  return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+
+  writeData(testData, testFileName);
+
+  // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+  expect(fs.writeFile).toBeCalled();
 });
