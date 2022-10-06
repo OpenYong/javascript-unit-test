0dbd5c2800434e33c3b78f5b3fdb0123d948c31c
OpenYong <fkszm3@gmail.com>
2022-10-06 17:32:41 +0900

    07-69 More Mocking Functionalities

--- 07-side-effects/src/data.test.js
+++ 07-side-effects/src/data.test.js
@@ -5,6 +5,9 @@ describe("generateReportData()", () => {
   it("logFn을 전달 받으면 실행시킨다.", () => {
     const logger = vi.fn();
 
+    // logger.mockImplementation(() => {});
+    // logger.mockImplementationOnce(() => {});
+
     generateReportData(logger);
 
     expect(logger).toBeCalled();
