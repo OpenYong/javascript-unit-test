8ef5f81235bee9557098737a0d0e5c10c56e89ce
OpenYong <fkszm3@gmail.com>
2023-06-01 23:59:10 +0900

    Writing a First Test

--- 03-basics/basic-testing-frontend/src/math.test.js
+++ 03-basics/basic-testing-frontend/src/math.test.js
@@ -1,1 +1,8 @@
-import { test } from "vitest";
+import { it, expect } from "vitest";
+
+import { add } from "./math.js";
+
+it("배열 안의 모든 숫자 값을 더한다.", () => {
+  const result = add([1, 2, 3, 4, 5]);
+  expect(result).toBe(15);
+});
