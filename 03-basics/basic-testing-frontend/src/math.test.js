6835078fe6460e537db127baf0fbb31826985dd4
OpenYong <fkszm3@gmail.com>
2023-06-04 13:46:42 +0900

    Testing For Errors

--- 03-basics/basic-testing-frontend/src/math.test.js
+++ 03-basics/basic-testing-frontend/src/math.test.js
@@ -47,6 +47,20 @@ it("빈 배열이 제공되었을때 0이 발생되게 된다.", () => {
 });
 
 it("함수에 매개 변수를 넣어주지 않았을 때 에러를 throw 한다.", () => {
-  const result = add();
-  // error는 return value가 아닌데 어떻게  테스트를 할까?
+  // Q:error는 return value가 아닌데 어떻게  테스트를 할까?
+  // A:try catch 로도 해결할 수 있다.
+
+  // 방법 1.
+  // try {
+  //   const result = add();
+  // } catch (error) {
+  //   expect(error).toBeDefined();
+  // }
+
+  // 방법 2.
+  const resultFn = () => {
+    add();
+  };
+
+  expect(resultFn).toThrow();
 });
