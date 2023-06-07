b945cb3c6df6309dda10b9b244a1011a3ed61d0c
OpenYong <fkszm3@gmail.com>
2023-06-07 09:20:53 +0900

    Tests With Multiple Assertions

--- 03-basics/basic-testing-frontend/src/util/numbers.test.js
+++ 03-basics/basic-testing-frontend/src/util/numbers.test.js
@@ -2,7 +2,7 @@ import { it, expect } from "vitest";
 
 import { transformToNumber } from "./numbers";
 
-it("숫자 문자열을 전달했을때 숫자를 반환한다", () => {
+it("숫자 문자열을 전달했을때 number 타입으로 변환하여  반환한다", () => {
   //Arrange
   const stringInput = "9";
 
@@ -13,10 +13,21 @@ it("숫자 문자열을 전달했을때 숫자를 반환한다", () => {
   expect(result).toBeTypeOf("number");
 });
 
-it("숫자 문자열이 아닌 일반 문자열이 전달됐을때 Undefined을 발생시킨다.", () => {
-  const stringInput = "숫자가 아닌";
+it("숫자 문자열을 전달했을때 number 타입으로 변환하여 반환한다", () => {
+  const stringInput = "9";
 
   const result = transformToNumber(stringInput);
 
+  expect(result).toBe(+stringInput);
+});
+
+it("숫자 문자열이 아닌 일반 문자열이 전달됐을때 NaN 발생시킨다.", () => {
+  const stringInput = "숫자가 아닌";
+  const input = {};
+
+  const result = transformToNumber(stringInput);
+  const result2 = transformToNumber(input);
+
   expect(result).toBeNaN();
+  expect(result2).toBeNaN();
 });
