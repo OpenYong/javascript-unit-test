0497934e0367df1290773cec9f61f62976c98848
OpenYong <fkszm3@gmail.com>
2023-06-14 22:37:42 +0900

    validation 함수 테스트 케이스 추가

--- 03-basics/basic-testing-frontend/src/util/validation.test.js
+++ 03-basics/basic-testing-frontend/src/util/validation.test.js
@@ -8,8 +8,32 @@ it("인풋이 숫자 값이 아닐때 에러를 발생시킨다.", () => {
   expect(validateFn).toThrow();
 });
 
-it("인풋이 숫자형일 떄 에러를 발생시키지 않는다..", () => {
+it("인풋이 숫자형일 떄 에러를 발생시키지 않는다.", () => {
   const input = 1;
   const validateFn = () => validateNumber(input);
   expect(validateFn).not.toThrow();
+});
+
+it("인풋이 NaN일 때 에러를 발생시킨다.", () => {
+  const input = NaN;
+  const validationFn = () => validateNumber(input);
+  expect(validationFn).toThrow();
+});
+
+it("인풋이 NaN일 때 에러메시지에 Invalid number를 포함시킨다.", () => {
+  const input = NaN;
+  const validationFn = () => validateNumber(input);
+  expect(validationFn).toThrow(/Invalid number/);
+});
+
+it("인풋이 빈 문자열일 때, 에러를 발생시킨다.", () => {
+  const input = "";
+  const validationFn = () => validateStringNotEmpty(input);
+  expect(validationFn).toThrow();
+});
+
+it("인풋이 빈 문자열일 때, must not be empty 라는 메시지를 포함한다.", () => {
+  const input = "";
+  const validationFn = () => validateStringNotEmpty(input);
+  expect(validationFn).toThrow(/must not be empty/);
 });
