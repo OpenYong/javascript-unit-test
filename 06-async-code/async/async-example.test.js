90a28f52ca6944e6e2878e1e0ba29884f6f3ab7d
OpenYong <fkszm3@gmail.com>
2023-06-22 23:27:14 +0900

    Testing Asynchronous Code With Callbacks

--- 06-async-code/async/async-example.test.js
+++ 06-async-code/async/async-example.test.js
@@ -1,26 +1,22 @@
 import { expect, it } from "vitest";
 import { generateToken, generateTokenPromise } from "./async-example";
 
-it("토근 값을 생성해낸다. callback", (done) => {
-  const userEmail = "test@test.com";
+it("토근 값을 생성해낸다. callback", () =>
+  new Promise((resolve, reject) => {
+    const userEmail = "test@test.com";
 
-  generateToken(userEmail, (err, token) => {
-    // expect(token).toBeDefined(); // Undefined가 아닌것을 기대함
+    generateToken(userEmail, (err, token) => {
+      // expect(token).toBeDefined(); // Undefined가 아닌것을 기대함
 
-    try {
-      expect(token).toBeDefined();
-      // expect(token).toBe(2);
-      done();
-    } catch (error) {
-      done(error);
-    }
-
-    // toBe(2) 도 테스트에 통과하고 있고,
-    // 실제로 작동하는 테스트 코드가 아니라고 봐야함.
-    // 고쳐보자!
-    // done();
-  });
-});
+      try {
+        // expect(token).toBeDefined();
+        expect(token).toBe(2);
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }));
 
 it("토근 값을 생성해낸다. Promise", () => {
   const userEmail = "test@test.com";
