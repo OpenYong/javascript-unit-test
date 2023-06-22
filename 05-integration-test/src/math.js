f042b3cab9f33ec7789adfc475422b18b2a61af7
OpenYong <fkszm3@gmail.com>
2023-06-22 22:11:47 +0900

    add integration tests section

--- 05-integration-test/src/math.js
+++ 05-integration-test/src/math.js
@@ -1,4 +1,4 @@
-import { cleanNumbers } from "./util/numbers.js";
+import { cleanNumbers } from './util/numbers.js';
 
 export function add(numbers) {
   let sum = 0;
@@ -12,10 +12,9 @@ export function add(numbers) {
 }
 
 export function calculateResult(numberValues) {
-  // 2.
-  let result = "";
+  let result = '';
   try {
-    const numbers = cleanNumbers(numberValues); // 3.
+    const numbers = cleanNumbers(numberValues);
 
     result = add(numbers).toString();
   } catch (error) {
