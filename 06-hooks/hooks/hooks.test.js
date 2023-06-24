6d0eda16bd9efea5503cb337b515e80e62262c91
OpenYong <fkszm3@gmail.com>
2023-06-24 18:10:35 +0900

    add 테스트 파일에 전역 변수 선언
    - 전역 변수 선언 후 테스트 실패 함
    -  test hook 사용하여 해결 가능

--- 06-hooks/hooks/hooks.test.js
+++ 06-hooks/hooks/hooks.test.js
@@ -2,50 +2,35 @@ import { it, expect } from "vitest";
 
 import { User } from "./hooks";
 
+// 코드 작성 반복을 줄이고 싶다면,
+// 전역 변수로도 설정이 가능하다.
+const name = "Lee";
+const age = 99;
+const user = new User(name, age);
+
 it("이름을 업데이트 한다.", () => {
-  const name = "Lee";
-  const age = 99;
   const newName = "Yong";
 
-  const user = new User(name, age);
   user.updateName(newName);
 
   expect(user.name).toBe(newName);
 });
 
 it("'name' 프로퍼티를 가져야한다.", () => {
-  const name = "Lee";
-  const age = 99;
-
-  const user = new User(name, age);
-
   expect(user).toHaveProperty("name");
 });
 
 it("전달된 name 값을 저장해야한다.", () => {
-  const name = "Lee";
-  const age = 99;
-
-  const user = new User(name, age);
-
   expect(user.name).toBe(name);
 });
 
 it("'name' 프로퍼티 값을 지워야한다.", () => {
-  const name = "Lee";
-  const age = 99;
-
-  const user = new User(name, age);
   user.deleteAge();
 
   expect(user.name).toBeNull;
 });
 
 it("deleteAge메소드 실행 후에도 'age' 프로퍼티를 가져야한다.", () => {
-  const name = "Lee";
-  const age = 99;
-
-  const user = new User(name, age);
   user.deleteAge();
 
   expect(user).toHaveProperty("age");
