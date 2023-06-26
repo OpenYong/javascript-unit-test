9bc110ccd83bbc91f19980907646b0e8206cc4a0
OpenYong <fkszm3@gmail.com>
2023-06-26 21:47:27 +0900

    add mocking 테스트 코드 추가

--- 07-mocking-spies/src/util/io.test.js
+++ 07-mocking-spies/src/util/io.test.js
@@ -1,10 +1,17 @@
-import { it, expect } from "vitest";
+import { it, expect, vi } from "vitest";
+import { promises as fs } from "fs";
 
 import writeData from "./io";
 
-it("writeData() 메소드를 실행시킨다.", () => {
+// vi.mock로 module import와 export를 mocking할 수 있다.
+vi.mock("fs");
+
+it("writeFile 메소드를 실행시킨다.", () => {
   const testData = "테스트 메시지";
   const testFilename = "test.txt";
 
-  return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+  writeData(testData, testFilename);
+
+  // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+  expect(fs.writeFile).toBeCalled();
 });
